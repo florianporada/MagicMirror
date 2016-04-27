4c1fc543030146466ffabc0f1acd08549c4be832
Florian Porada <florian.porada@gmail.com>
2016-04-27 17:52:30 +0200

    config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,3 @@
-/* exported config */
-
 /* Magic Mirror Config
  *
  * By Michael Teeuw http://michaelteeuw.nl
@@ -7,65 +5,71 @@
  */
 
 var config = {
-	port: 8080,
+    port: 8080,
 
-	language: 'de',
-	timeFormat: 24,
+    language: 'en',
+    timeFormat: 24,
+    units: 'metric',
 
-	modules: [
-		{
-			module: 'alert',
-		},
-		{
-			module: 'clock',
-			position: 'top_left'
-		},
-		{
-			module: 'calendar',
-			header: 'US Holidays',
-			position: 'top_left',
-			config: {
-				calendars: [
-					{
-						symbol: 'calendar-check-o ',
-						url: 'webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics'
-					}
-				]
-			}
-		},
-		{
-			module: 'compliments',
-			position: 'lower_third'
-		},
-		{
-			module: 'currentweather',
-			position: 'top_right',
-			config: {
-				location: 'Schondra',
-				appid: 'a4739f8834ed58759d4d3adc7ae7ff8c'
-			}
-		},
-		{
-			module: 'weatherforecast',
-			position: 'top_right',
-			header: 'Weather Forecast',
-			config: {
-	            location: 'Schondra',
-	            appid: 'abf8ee9c8ddb8123f579247987b94d8f'
-			}
-		},
-		{
-			module: 'newsfeed',
-			position: 'bottom_bar',
-			config: {
-				feedUrl: 'http://www.spiegel.de/schlagzeilen/tops/index.rss',
-				showPublishDate: true,
-			}
-		},
-	]
+    modules: [
+        {
+            module: 'alert',
+        },
+        {
+            module: 'clock',
+            position: 'top_left'
+        },
+        {
+            module: 'calendar',
+            header: 'German Holidays',
+            position: 'top_left',
+            config: {
+                calendars: [
+                    {
+                        symbol: 'calendar-check-o ',
+                        url: 'webcal://www.calendarlabs.com/templates/ical/Germany-Holidays.ics'
+                    }
+                ]
+            }
+        },
+        {
+            module: 'compliments',
+            position: 'lower_third'
+        },
+        {
+            module: 'currentweather',
+            position: 'top_right',
+            config: {
+                location: 'Schondra',
+                appid: 'a4739f8834ed58759d4d3adc7ae7ff8c'
+            }
+        },
+        {
+            module: 'weatherforecast',
+            position: 'top_right',
+            header: 'Weather Forecast',
+            config: {
+                location: 'Schondra',
+                appid: 'a4739f8834ed58759d4d3adc7ae7ff8c'
+            }
+        },
+        {
+            module: 'newsfeed',
+            position: 'bottom_bar',
+            config: {
+                feeds: [
+                    {
+                        title: "Spiegel Online",
+                        url: "http://www.spiegel.de/schlagzeilen/tops/index.rss"
+                    }
+                ],
+                showSourceTitle: true,
+                showPublishDate: true
+            }
+        },
+    ]
 
 };
-
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
 if (typeof module !== 'undefined') {module.exports = config;}
